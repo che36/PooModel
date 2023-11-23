10b4df88b0299c2c5961da3c6e5159681c6e7916
Neo40 <tron36.cc@gmail.com>
2023-11-22 22:01:43 -0300

    Setup inicial da classe Endereco

---
+++ src/poo.model/Endereco.java
@@ -0,0 +1,12 @@
+package poo.model;
+
+public class Endereco {
+
+    public String endereco;
+    public String numero;
+    public String complemento;
+    public String cidade;
+    public String estado;
+    public String cep;
+
+}
