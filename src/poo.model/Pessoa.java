51c71c63f31a39c462c32e08aa0377c3b71a4392
Neo40 <tron36.cc@gmail.com>
2023-11-23 14:30:01 -0300

    Criação de classe Pessoa com enum e atributos

--- src/poo.model/Pessoa.java
+++ src/poo.model/Pessoa.java
@@ -1,9 +1,10 @@
 package poo.model;
 
 public class Pessoa {
+    public enum TipoPessoa {FISICA, JURIDICA}
 
     public Integer codigo;
     public String documento;
     public String nome;
-
+    public TipoPessoa tipo;
 }
