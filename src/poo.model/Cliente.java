64988edf8379fbb81ff63b1a07afe149f702307e
Neo40 <tron36.cc@gmail.com>
2023-11-23 14:26:31 -0300

    Remoçao de atributos

--- src/poo.model/Cliente.java
+++ src/poo.model/Cliente.java
@@ -2,10 +2,6 @@ package poo.model;
 
 public class Cliente {
 
-    public Integer codigo;
-    public String nome;
-    public String cpf;
-
     public List<Endereco> enderecos;
 
     public void adicionarEndereco(Endereco endereco) {
